00421c8ff7434514c6b5f8bf9b86b1bce71d21e1
mezitomi <mezitomi@gmail.com>
2018-07-12 12:16:26 +0200

    refactored queries

--- src/main/java/com/codecool/shop/db/Queries.java
+++ src/main/java/com/codecool/shop/db/Queries.java
@@ -10,15 +10,33 @@ import java.util.Map;
 
 public class Queries {
 
+    private static Product parseProductFromMap(Map<String, Object> product) {
+        return new Product(product.get("name").toString(),
+                Float.parseFloat(product.get("price").toString()),
+                product.get("currency").toString(),
+                product.get("description").toString(),
+                Queries.getCategoryById(Integer.parseInt(product.get("category").toString())),
+                Queries.getSupplierById(Integer.parseInt(product.get("supplier").toString())));
+    }
+
+    private static Supplier parseSupplierFromMap(Map<String, Object> supplier) {
+        return new Supplier(Integer.parseInt(supplier.get("id").toString()),
+                supplier.get("name").toString(),
+                supplier.get("description").toString());
+    }
+
+    private static ProductCategory parseProductCategoryFromMap(Map<String, Object> category) {
+        return new ProductCategory(
+                Integer.parseInt(category.get("id").toString()),
+                category.get("name").toString(),
+                category.get("department").toString(),
+                category.get("description").toString());
+    }
+
     private static List<Product> parseMapListOfProducts(List<Map<String, Object>> mapList) {
         List<Product> productList = new ArrayList<>();
-        for (Map<String, Object> row: mapList) {
-            productList.add(new Product(row.get("name").toString(),
-                    Float.parseFloat(row.get("price").toString()),
-                    row.get("currency").toString(),
-                    row.get("description").toString(),
-                    Queries.getCategoryById(Integer.parseInt(row.get("category").toString())),
-                    Queries.getSupplierById(Integer.parseInt(row.get("supplier").toString()))));
+        for (Map<String, Object> row : mapList) {
+            productList.add(parseProductFromMap(row));
         }
         return productList;
     }
@@ -26,25 +44,16 @@ public class Queries {
     public static List<Product> getAllProducts() {
         String query = "Select * from product";
         return parseMapListOfProducts(ConnectToDB.executeQuery(query));
-
     }
 
     public static Supplier getSupplierById(int id) {
-       String query = "SELECT * FROM supplier WHERE id = " + id;
-        Map<String, Object> result = ConnectToDB.executeQuery(query).get(0);
-        return new Supplier(Integer.parseInt(result.get("id").toString()),
-                            result.get("name").toString(),
-                            result.get("description").toString());
+        String query = "SELECT * FROM supplier WHERE id = " + id;
+        return parseSupplierFromMap(ConnectToDB.executeQuery(query).get(0));
     }
 
     public static ProductCategory getCategoryById(int id) {
         String query = "SELECT * FROM product_category WHERE id = " + id;
-        Map<String, Object> result = ConnectToDB.executeQuery(query).get(0);
-        return new ProductCategory(
-                Integer.parseInt(result.get("id").toString()),
-                result.get("name").toString(),
-                result.get("department").toString(),
-                result.get("description").toString());
+        return parseProductCategoryFromMap(ConnectToDB.executeQuery(query).get(0));
     }
 
     public static List<Product> getProductsByCategory(ProductCategory category) {
@@ -55,6 +64,11 @@ public class Queries {
     public static List<Product> getProductsBySupplier(Supplier supplier) {
         String query = "SELECT * FROM product WHERE supplier = " + supplier.getId();
         return parseMapListOfProducts(ConnectToDB.executeQuery(query));
+    }
+
+    public static Product findProductById(int id) {
+        String query = "SELECT * FROM product WHERE id = " + id;
+        return parseProductFromMap(ConnectToDB.executeQuery(query).get(0));
     }
 
 }
